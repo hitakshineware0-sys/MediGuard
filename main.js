9cd4ba0a5eb883b369a7f826611f02b4ea688338
hitakshineware0-sys <hitakshineware0@gmail.com>
2026-03-16 01:19:07 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+import React from "https://esm.sh/react@18"
+import ReactDOM from "https://esm.sh/react-dom@18/client"
+
+function App() {
+  return (
+    React.createElement("div", null, "MediTrack App Running")
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(React.createElement(App))
